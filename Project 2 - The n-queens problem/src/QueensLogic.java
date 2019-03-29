7d5d88046083ea155d63361a78ebab1be7a7a28a
stei <stei@itu.dk>
2019-03-29 15:40:55 +0100

    Done

--- Project 2 - The n-queens problem/src/QueensLogic.java
+++ Project 2 - The n-queens problem/src/QueensLogic.java
@@ -8,15 +8,13 @@ public class QueensLogic implements IQueensLogic {
     private BDDFactory fact;
     private BDD True;
     private BDD False;
-    private BDD bdd;
+    private BDD rootBDD;
 
 
     @Override
     public void initializeBoard(int size) {
         this.size = size;
         board = new int[size][size];
-
-        // Initializing all positions on the board
 
         InitializeBDD();
         updateInvalidPositions();
@@ -30,49 +28,75 @@ public class QueensLogic implements IQueensLogic {
     @Override
     public void insertQueen(int column, int row) {
 
+        // Check whether position is empty
         if (board[column][row] == -1 || board[column][row] == 1) {
+
+            // If not, just return
             return;
         }
+
+        // Place a Queen on given position
         board[column][row] = 1;
 
-        bdd = bdd.restrict(getVariable(column, row));
+        // Updates the rootBDD by the new restriction of the new placed
+        // queen
+        rootBDD = rootBDD.restrict(getVariable(column, row));
 
+        // Updates the board
         updateInvalidPositions();
     }
 
+    /**
+     * Method that initialize the process of building the BDD
+     */
     private void InitializeBDD() {
-        int nVars = size*size;
 
+        // Initialize the fields
         fact = JFactory.init(2000000,200000);
         True = fact.one();
         False = fact.zero();
-        bdd = True;
+        rootBDD = True;
 
+        // The amount of variables are the amount of
+        // position on the board
+        int nVars = size*size;
+
+        // Initialize the variable number in the BDD factory
         fact.setVarNum(nVars);
 
-        for (int column = 0; column < board.length; column++) {
-            for (int row = 0; row < board[column].length; row++) {
-                createRuleForPosition(column, row);
-            }
-        }
-
-        for (int row = 0; row < size; row++) {
-            BDD sub_bdd = False;
-
-            for (int column = 0; column < size; column++) {
-                sub_bdd = sub_bdd.or(getVariable(column, row));
-            }
-
-            //sub_bdd must be true
-            bdd = bdd.and(sub_bdd);
-        }
+        // Build the BDD by creating the rules for all
+        // variables/positions
+        createRules();
     }
 
-    private void createRuleForPosition(int column, int row) {
+    /**
+     * Method that creates the rules in the BDD in the correct order
+     */
+    private void createRules() {
+        // Create rule for the individual position, affects columns,
+        // rows and diagonal, since that is how Queens behave
+        for (int column = 0; column < board.length; column++) {
+            for (int row = 0; row < board[column].length; row++) {
+                createRuleForQueensPosition(column, row);
+            }
+        }
+
+        // Create rule that ensure that there can be a queen on
+        // every row
+        createQueenOnEveryColumnRule();
+    }
+
+    /**
+     * Method that for every given position on given column and row,
+     * create the rules connected to that position
+     */
+    private void createRuleForQueensPosition(int column, int row) {
         BDD n = True;
         BDD subBDD = False;
 
+
         // All column cells on rows position should be added
+        // left --> right
         for (int c = 0; c < size; c++) {
             if(column != c) {
                 // It should be 'and', since if one queen is placed, the
@@ -82,6 +106,7 @@ public class QueensLogic implements IQueensLogic {
         }
 
         // All row cells on columns position should be added
+        // top --> bottom
         for (int r = 0; r < size; r++) {
             if(row != r) {
                 n = n.and(getNotVariable(column, r));
@@ -89,8 +114,7 @@ public class QueensLogic implements IQueensLogic {
         }
 
 
-        int c = column;
-        int r = row;
+        int c = column; int r = row;
 
         // Diagonal top left --> bottom right
         while (r < size && c < size) {
@@ -104,7 +128,6 @@ public class QueensLogic implements IQueensLogic {
 
         int c2 = column; int r2 = row;
 
-
         // Diagonal bottom left --> top right
         while (r2 >= 0 && c2 < size) {
 
@@ -113,14 +136,39 @@ public class QueensLogic implements IQueensLogic {
             }
 
             c2++; r2--;
-
         }
 
+        // Since subBDD initially is false, then the negated position
+        // at given column and row decide if it is true
         subBDD = subBDD.or(getNotVariable(column, row));
 
+        // If given position is true, then all positions in the given rules
+        // is true
         subBDD = subBDD.or(n);
 
-        bdd = bdd.and(subBDD);
+        // Link it together with all other rules
+        rootBDD = rootBDD.and(subBDD);
+    }
+
+    /**
+     * Method that creates the rule that ensures a queen
+     * must be able to be placed at every row
+     */
+    private void createQueenOnEveryColumnRule() {
+
+        // There must be a queen on every row, this rule checks
+        // that for given position, if there can't be a queen on
+        // every row, the position is invalid
+        for (int row = 0; row < size; row++) {
+            BDD subBDD = False;
+
+            for (int column = 0; column < size; column++) {
+                subBDD = subBDD.or(getVariable(column, row));
+            }
+
+            //sub_bdd must be true
+            rootBDD = rootBDD.and(subBDD);
+        }
     }
 
     /**
@@ -138,12 +186,15 @@ public class QueensLogic implements IQueensLogic {
     }
 
     /**
-     * Helper method that
+     * Helper method that checks whether the given position is
+     * invalid
      */
     private boolean isPositionInvalid(int column, int row) {
-        BDD position = bdd.restrict(getVariable(column, row));
+        // Creates a new BDD restricted to given position
+        BDD positionBDD = rootBDD.restrict(getVariable(column, row));
 
-        return position.isZero();
+        // If position is invalid, return true
+        return positionBDD.isZero();
     }
 
     /**
@@ -152,13 +203,16 @@ public class QueensLogic implements IQueensLogic {
      */
     private void updateInvalidPositions() {
 
+        // Counter to help determine the remaining valid positions
         int invalidCellsCounter = 0;
 
         for (int column = 0; column < board.length; column++) {
             for (int row = 0; row < board[column].length; row++) {
                 if (isPositionInvalid(column, row)) {
 
+                    // If position is invalid, mark it in the board
                     board[column][row] = -1;
+
                     invalidCellsCounter++;
                 }
             }
@@ -166,7 +220,6 @@ public class QueensLogic implements IQueensLogic {
 
         // Checking the remaining valid positions
         checkRemainingValidPositions(invalidCellsCounter);
-
     }
 
     /**
@@ -174,10 +227,12 @@ public class QueensLogic implements IQueensLogic {
      * are equal to size
      */
     private void checkRemainingValidPositions(int invalidCells) {
+
         int validCells = size*size - invalidCells;
 
         if(validCells == size) {
-            // if true then on the remaining valid positions a Queen
+
+            // If true then on the remaining valid positions a Queen
             // should be placed
             for (int column = 0; column < board.length; column++) {
                 for (int row = 0; row < board[column].length; row++) {
@@ -189,4 +244,3 @@ public class QueensLogic implements IQueensLogic {
         }
     }
 }
-
